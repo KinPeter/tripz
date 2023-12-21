828831e15649f28f32e3bd18ee04c4797269fb1e
Peter Kin <kinpeter85@gmail.com>
2023-12-21 21:33:09 +0200

    Prep: add ids to entries

---
+++ prep-scripts/flights/temp/add-uuids-joins.js
@@ -0,0 +1,53 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { v4 as uuid } from 'uuid';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+// Resolve the absolute path to the CSV file using __dirname
+const airportPath = path.resolve(__dirname, '../airportsWithIds.json');
+const airlinesPath = path.resolve(__dirname, '../airlinesWithIds.json');
+const aircraftsPath = path.resolve(__dirname, '../aircraftsWithIds.json');
+const flightsInputPath = path.resolve(__dirname, 'flights.json');
+const flightsOutputPath = path.resolve(__dirname, '../flightsWithIds.json');
+const flightsBundleOutputPath = path.resolve(__dirname, '../flightsBundleWithIds.json');
+
+try {
+  // Read the json file synchronously
+  const airports = JSON.parse(fs.readFileSync(airportPath, 'utf-8'));
+  const airlines = JSON.parse(fs.readFileSync(airlinesPath, 'utf-8'));
+  const aircrafts = JSON.parse(fs.readFileSync(aircraftsPath, 'utf-8'));
+  const flightsInput = JSON.parse(fs.readFileSync(flightsInputPath, 'utf-8'));
+
+  const flights = flightsInput.map(flight => {
+    return {
+      id: uuid(),
+      ...flight,
+      from: airports.find(({ iata }) => iata === flight.from)?.id ?? 'error',
+      to: airports.find(({ iata }) => iata === flight.to)?.id ?? 'error',
+      airline: airlines.find(({ iata }) => iata === flight.airline)?.id ?? 'error',
+      aircraft: aircrafts.find(({ icao }) => icao === flight.aircraft)?.id ?? 'error',
+    };
+  });
+
+  const flightsBundle = flightsInput.map(flight => {
+    return {
+      id: uuid(),
+      ...flight,
+      from: airports.find(({ iata }) => iata === flight.from) ?? 'error',
+      to: airports.find(({ iata }) => iata === flight.to) ?? 'error',
+      airline: airlines.find(({ iata }) => iata === flight.airline) ?? 'error',
+      aircraft: aircrafts.find(({ icao }) => icao === flight.aircraft) ?? 'error',
+    };
+  });
+
+  fs.writeFileSync(flightsOutputPath, JSON.stringify(flights, null, 2));
+  fs.writeFileSync(flightsBundleOutputPath, JSON.stringify(flightsBundle, null, 2));
+
+  // Process the parsed CSV data here
+  console.log('JSON file successfully processed');
+} catch (error) {
+  console.error('Error reading JSON file:', error.message);
+}
---
+++ prep-scripts/flights/temp/add-uuids.js
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { v4 as uuid } from 'uuid';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+// Resolve the absolute path to the CSV file using __dirname
+const airportInputPath = path.resolve(__dirname, 'airportsWithCoords.json');
+const airportOutputPath = path.resolve(__dirname, 'airportsWithIds.json');
+const airlinesInputPath = path.resolve(__dirname, 'airlines.json');
+const airlinesOutputPath = path.resolve(__dirname, 'airlinesWithIds.json');
+const aircraftsInputPath = path.resolve(__dirname, 'aircrafts.json');
+const aircraftsOutputPath = path.resolve(__dirname, 'aircraftsWithIds.json');
+
+try {
+  // Read the json file synchronously
+  const airportsInput = JSON.parse(fs.readFileSync(airportInputPath, 'utf-8'));
+
+  const airports = airportsInput.map(input => ({
+    ...input,
+    id: uuid(),
+  }));
+
+  fs.writeFileSync(airportOutputPath, JSON.stringify(airports, null, 2));
+
+  const airlinesInput = JSON.parse(fs.readFileSync(airlinesInputPath, 'utf-8'));
+
+  const airlines = airlinesInput.map(input => ({
+    ...input,
+    id: uuid(),
+  }));
+
+  fs.writeFileSync(airlinesOutputPath, JSON.stringify(airlines, null, 2));
+
+  const aircraftsInput = JSON.parse(fs.readFileSync(aircraftsInputPath, 'utf-8'));
+
+  const aircrafts = aircraftsInput.map(input => ({
+    ...input,
+    id: uuid(),
+  }));
+
+  fs.writeFileSync(aircraftsOutputPath, JSON.stringify(aircrafts, null, 2));
+
+  // Process the parsed CSV data here
+  console.log('JSON file successfully processed');
+} catch (error) {
+  console.error('Error reading JSON file:', error.message);
+}
