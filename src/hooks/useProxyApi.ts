3f6c878653a4718520244f4f8d616c1eeb8c6b56
Peter Kin <kinpeter85@gmail.com>
2025-07-19 20:52:16 +0200

    Remove old api functions

--- src/hooks/useProxyApi.ts
+++ src/hooks/useProxyApi.ts
@@ -1,20 +1,12 @@
 import { ApiClient } from '../lib/apiClient.ts';
-import { Airport, Airline, VisitRequest } from '../types';
+import { VisitRequest } from '../types';
 
 export const useProxyApi = () => {
   const api = new ApiClient();
-
-  async function getAirport(iata: string): Promise<Airport> {
-    return await api.get('/proxy/airport/' + iata);
-  }
-
-  async function getAirline(iata: string): Promise<Airline> {
-    return await api.get('/proxy/airline/' + iata);
-  }
 
   async function getCity(lat: number, lng: number): Promise<VisitRequest> {
     return await api.get('/proxy/location/city/?lat=' + lat + '&lng=' + lng);
   }
 
-  return { getAirport, getAirline, getCity };
+  return { getCity };
 };
